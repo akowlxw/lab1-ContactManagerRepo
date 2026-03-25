a61f4be1e007fbe516be30be10da68ad8346be12
kovaleva <kovaleva@mail.com>
2026-03-25 18:44:31 +0300

    Добавлен класс Contact

---
+++ ContactManagerProject/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace ContactManagerProject
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public Contact(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+
+}

7abf86519751e9dea5ba11557415bf08352c93be
kovaleva <kovaleva@mail.com>
2026-03-25 18:52:07 +0300

    Создан графический интерфейс с элементами управления

--- ContactManagerProject/Program.cs
+++ ContactManagerProject/Program.cs
@@ -16,7 +16,7 @@ namespace ContactManagerProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ContactForm());
         }
     }
 }
